96e6ff0cc35f6813ad4dde67c75183479c6737a7
WangYizhi <wangyz.xjtu@gmail.com>
2020-12-20 20:39:39 +0800

    2020-12-20

--- 402.移掉k位数字.cpp
+++ 402.移掉k位数字.cpp
@@ -8,26 +8,40 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        vector<int> count(10);
-        bool used[10];
-        memset(used,false,sizeof(used));
+        if(num.size() <= k) return "0";
         string res = "";
 
         for(auto c : num){
-            count[atoi(c)] ++;
-        }
-        for(auto c : s){
-            count[atoi(c)]--;
-            if(used[c-'a']) continue;
-            while( res.length()>0&& c < res[res.length()-1] && count[res[res.length()-1] - 'a']>0){
-                used[res[res.length()-1] - 'a'] = false;
+            while( res.length()>0&& c < res[res.length()-1] && k > 0){
                 res.erase(res.length()-1);
+                k--;
             }
             res += c;
-            used[c-'a']=true;
         }
-        return res;
-    }
+        // cout<< "k: " << k <<endl;
+        int count = 0;
+        for(auto i = res.begin() ; i != res.end()  ; ++i){
+            if(*i == '0') {
+                count++;
+            }else{
+                // res.erase(i);
+                break;
+            }
+        }
+        // int len = res.length();
+        // res.substr(count,len-count);
+        res.erase(0,count);
+        while( k != 0){
+            res.pop_back();
+            k--;
+        }
+        if(res.length()>0){
+            return res;
+        }else{
+            return "0";
+        }
+        // cout<< count <<endl;
+        // return res;
     }
 };
 // @lc code=end
