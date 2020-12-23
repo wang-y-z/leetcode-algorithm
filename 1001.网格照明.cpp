12460142d6d96871e1c0464746b22492c96d5b74
wang-y-z <wangyz.xjtu@gmail.com>
2020-12-24 00:39:37 +0800

    2020-12-24

---
+++ 1001.网格照明.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1001 lang=cpp
+ *
+ * [1001] 网格照明
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> gridIllumination(int N, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
+
+    }
+};
+// @lc code=end
+
