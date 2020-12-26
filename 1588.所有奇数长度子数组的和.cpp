44d80a737eba0dfcea33a0e6df35c95a7a6c3e62
wang-y-z <wangyz.xjtu@gmail.com>
2020-12-27 01:18:50 +0800

    2020-12-27

---
+++ 1588.所有奇数长度子数组的和.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1588 lang=cpp
+ *
+ * [1588] 所有奇数长度子数组的和
+ */
+
+// @lc code=start
+//拿草稿纸画一下再说
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int res = 0;
+        int size = arr.size();
+        int iloop = (size+1)/2;
+        int index[size] = {0};
+        for(int i = 0;i<iloop ;++i){
+            for(int j = 0;j< ; j++){
+
+            }
+        }
+    }
+};
+// @lc code=end
+
