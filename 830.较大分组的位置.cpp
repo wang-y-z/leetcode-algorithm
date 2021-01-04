84ec5710398242d36d157ef05bf2cc9dcdc2fcc2
wang-y-z <wangyz.xjtu@gmail.com>
2021-01-05 00:42:25 +0800

    2021-01-05

--- 830.较大分组的位置.cpp
+++ 830.较大分组的位置.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
-
+        vector<vector<int>> res;
+        int count = 0;
+        int start = 0;
+        for(int i = 1 ; i<s.length();i++){
+            if(s[i-1] == s[i] ){
+                count++;
+                if(count>=2&&i==s.length()-1){
+                    res.push_back({start,start+count});
+                }
+            }else{
+                if(count>=2){
+                    res.push_back({start,start+count});
+                    start = i;
+                    count = 0;
+                }else{
+                    start = i;
+                    count = 0;    
+                }
+            }
+        }    
+        return res;
     }
 };
 // @lc code=end
