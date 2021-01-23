49e28526c4f258907d975f3d4b86a5bf4c4b18cc
wang-y-z <wangyz.xjtu@gmail.com>
2021-01-23 20:55:49 +0800

    2021-01-23

--- 206.反转链表.cpp
+++ 206.反转链表.cpp
@@ -15,16 +15,25 @@
  */
 class Solution {
 public:
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode * pre = NULL;
+    //     ListNode * cur = head;
+    //     while( cur ){
+    //         ListNode * tmp = cur->next;
+    //         cur->next = pre;
+    //         pre = cur;
+    //         cur = tmp;
+    //     }   
+    //     return pre;     
+    // }
+    //02 递归实现
     ListNode* reverseList(ListNode* head) {
-        ListNode * pre = NULL;
-        ListNode * cur = head;
-        while( cur ){
-            ListNode * tmp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = tmp;
-        }   
-        return pre;     
+        if(!head) return nullptr;//base case
+        if(!head->next) return head;//递归终止条件
+        ListNode* last = reverseList(head->next);//递归
+        head->next->next = head;//将head指向的东西指向head
+        head->next = nullptr;//将Head下一个指向null
+        return last; 
     }
 };
 // @lc code=end
--- 92.反转链表-ii.cpp
+++ 92.反转链表-ii.cpp
@@ -17,21 +17,42 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int m, int n) {
-        if(!head || !head->next || m==n) return head;
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode * pre = dummy;
-        int count = 0;
-        for( int i = 0 ; i < m-1 ; i++) pre = pre->next;
-        ListNode * cur = pre->next;
-        for( int i = m ; i < n ; i++ ){
-            ListNode * tmp = cur->next;
-            cur->next = tmp->next;
-            tmp->next = pre->next;
-            pre->next = tmp;
+    // ListNode* reverseBetween(ListNode* head, int m, int n) {
+    //     if(!head || !head->next || m==n) return head;
+    //     ListNode* dummy = new ListNode(0);
+    //     dummy->next = head;
+    //     ListNode * pre = dummy;
+    //     int count = 0;
+    //     for( int i = 0 ; i < m-1 ; i++) pre = pre->next;
+    //     ListNode * cur = pre->next;
+    //     for( int i = m ; i < n ; i++ ){
+    //         ListNode * tmp = cur->next;
+    //         cur->next = tmp->next;
+    //         tmp->next = pre->next;
+    //         pre->next = tmp;
+    //     }
+    //     return dummy->next;
+    // }
+
+    //02 递归解法
+    ListNode* reverseBetween(ListNode* head, int m , int n ){
+        if(m == 1){
+            return reverseN(head,n);
         }
-        return dummy->next;
+        head->next = reverseBetween(head->next , m -1 ,n -1);
+        return head;
+    }
+    ListNode* backnode = nullptr;
+    ListNode * reverseN(ListNode* head , int n){
+        if(!head) return nullptr;//base case
+        if(!head->next || n == 1){
+            backnode = head->next;
+            return head;//递归终止条件
+        }
+        ListNode* last = reverseN(head->next , n-1);//递归
+        head->next->next = head;//将head指向的东西指向head
+        head->next = backnode;//将Head下一个指向null
+        return last; 
     }
 };
 // @lc code=end
