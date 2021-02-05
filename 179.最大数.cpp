aa7d25835ce7c161e813bd0a7c4fcff77fb56f14
wang-y-z <wangyz.xjtu@gmail.com>
2021-02-05 09:23:00 +0800

    2021-02-05

--- 179.最大数.cpp
+++ 179.最大数.cpp
@@ -7,13 +7,16 @@
 // @lc code=start
 class Solution {
 public:
+    //01 自己想的，不断debug发现a+b和b+a定义大小就可以；
+    
+    // 注：a和b也可以不用传引用（即 &）
+    // 注：此处要用static，因为std::sort是属于全局的，无法调用非静态成员函数，而静态成员函数或全局函数是不依赖于具体对象，可以独立访问。
+    // 也可以把comparison这个函数放在Solution这个class的外面，但是记住一定要放在整个class的上面而不能是下面。
+    // 不然代码里调用sort函数时会找不到comparison，而导致报错。
     static bool cmp( int a , int b ){
         if(a == b) return true;
         string s1 = to_string(a) + to_string(b);
         string s2 = to_string(b) + to_string(a);
-        int i = 0;
-        // cout<<s1<<" " <<s2<<endl;
-        // cout<<s1<<" " <<s2<<endl;
         for(int i = 0 ; i < s1.size() ; i++){
             if(s1[i] < s2[i]){
                 return false;
@@ -23,13 +26,20 @@ public:
         }
         return true; 
     }
+    static bool cmp_string(  int a , int b ){
+        string s1 = to_string(a);
+        string s2 = to_string(b);
+        return s1+s2>s2+s1;
+    }
     string largestNumber(vector<int>& nums) {
         bool flag = true;
         for(auto i:nums){
             if(i != 0) flag = false;
         }
         if(flag == true) return "0";
-        std::sort(nums.begin(),nums.end(),cmp);
+        std::sort(nums.begin(),nums.end(),cmp_string);
+        
+        // std::sort(nums.begin(),nums.end(),cmp);
         string tmp;
         for(auto i : nums){
             if(tmp.size()==0 && i == 0){
@@ -37,9 +47,9 @@ public:
             }
             tmp += to_string(i);
         }
-        // if(tmp[0] == '0' && tmp.size() > 1) return "0";
         return tmp;
     }
+    //02 大佬解法: 跟大佬想的一样，开心；
+
 };
 // @lc code=end
-
