5164e639c4ce6a231f7d6f3d6996aaabb2ba33bf
wang-y-z <wangyz.xjtu@gmail.com>
2021-02-17 17:26:06 +0800

    2021-02-17

---
+++ 204.计数质数.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=cpp
+ *
+ * [204] 计数质数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2) return 0;
+        vector<bool> prime(n,true);
+        int count = n - 2;
+        for(int i = 2; i<n;i++){
+            if(prime[i]){
+                for(int j = 2*i ; j < n ; j+=i){
+                    if(prime[j]){
+                        prime[j] = false;
+                        --count;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
+// @lc code=end
+
